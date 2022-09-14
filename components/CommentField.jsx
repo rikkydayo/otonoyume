a8c7f12a5a42739fcfddf8e4b23619fbf760155b
AsoTaro <rickyshaun1230@gmail.com>
2022-09-14 23:12:21 +0900

    add comment

--- components/CommentField.jsx
+++ components/CommentField.jsx
@@ -1,43 +1,71 @@
 import { addComments, getComments } from "../libs/comment";
-import { useForm} from "react-hook-form"; // SubmitHandlerは、submitイベントに関する関数の型宣言に使う
+import { useForm } from "react-hook-form"; // SubmitHandlerは、submitイベントに関する関数の型宣言に使う
 import { ErrorMessage } from "@hookform/error-message";
-import { text } from "@fortawesome/fontawesome-svg-core";
+import { supabase } from "../libs/supabase";
+import { useLayoutEffect, useState } from "react";
+import styles from "../styles/Home.module.scss";
 
-export const CommentsField = ({blogId}) => {
+export const CommentsField = ({ blogId }) => {
   const {
     register, //inputなどに入力された値を参照するために使う
     handleSubmit,
     formState: { errors, isValid },
   } = useForm();
-//   const comments = getComments(blogId);
-  const onSubmit =  (data) => {
-    console.log(data);
+
+  const [state, setState] = useState([]);
+  useLayoutEffect(() => {
+    const comment = async () => {
+      const { data, error, status } = await supabase
+        .from("comment_tbl")
+        .select()
+        .eq("blog_id", blogId);
+
+      if (error && status !== 406) {
+        throw error;
+      }
+      setState(...state, data);
+    };
+    comment();
+  }, []);
+
+  const onSubmit = (data) => {
     const result = window.confirm("送信しますか？");
     if (result) {
-      addComments(data.content,userName=data.name,blogId=blogId);
+      addComments(data.name, data.content, blogId);
+      document.location.reload();
     }
   };
   return (
     <div>
       <div>
-        <p className="TableOfContentsHead">コメント欄</p>
+        <p className="text-xl font-bold text-gray-900">コメント欄：β版</p>
         <ul>
-          {/* {comments.map((comment) => (
-            <li key={data.id}>
-              <a>{comment.userName}</a>
-              <a>{comment.body}</a>
-            </li>
-          ))} */}
+          {state.map((elm) => {
+            return (
+              <li key={elm.id}>
+                <div className={styles.balloon}>
+                  <p className="whitespace-pre-line">
+                    {elm.user_name} : {elm.body}
+                  </p>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-unit">
+        <form
+          className="grid grid-cols-1 gap-6 m-16"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="block">
             <p className="form-unit-title">お名前</p>
             <input
-            type="text"
+              className="rounded-md w-full py-2 border-b focus:outline-none focus:border-b-2 focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50"
+              type="text"
               {...register("name", {
-                max:20,
+                max: 20,
+                required: "お名前は必須項目です。",
               })}
             />
             <ErrorMessage
@@ -50,9 +78,10 @@ export const CommentsField = ({blogId}) => {
               }
             />
           </div>
-          <div className="form-unit">
+          <div className="block">
             <p className="form-unit-title">コメント</p>
             <textarea
+              className="rounded-md w-full py-2 border-b focus:outline-none focus:border-b-2 focus:border-indigo-500 placeholder-gray-500 placeholder-opacity-50"
               placeholder="コメント内容を入力"
               {...register("content", {
                 required: "コメント内容は必須項目です。",
@@ -69,18 +98,12 @@ export const CommentsField = ({blogId}) => {
             />
           </div>
           <div className="form-actionArea">
-            {!isValid && (
-              <>
-                <p className="form-validateMessage">
-                  まだ全ての必須項目の入力が完了していません。
-                </p>
-              </>
-            )}
-            <div className="form-buttonWrapper">
-              <button type="submit" className="form-submitButton">
-                入力内容を確認する
-              </button>
-            </div>
+            <button
+              className="shadow bg-blue-500 hover:bg-yellow-300 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              送信
+            </button>
           </div>
         </form>
       </div>
